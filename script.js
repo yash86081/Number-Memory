fb56e363f8b855f0c03bec230df4cdea50ca7e6b
yash86081 <yashpandey860822@gmail.com>
2026-09-10 23:19:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let level = document.getElementById("level")
+let Number = document.getElementById("Number")
+let Input = document.getElementById("Input")
+let Submit = document.getElementById("Submit")
+let loss = document.getElementById("loss")
+let process = 0;
+let oldtimer=0;
+let currentLevel = 1;
+function resetNumber() {
+    Number.textContent=""
+    Input.style.display = "inline-block"
+    Submit.style.display = "inline-block"
+}
+level.textContent= "Level: "+currentLevel
+function produce() {
+    process= Math.floor(Math.random()*9*(10**(currentLevel+1))+10**(currentLevel+1))
+    Number.textContent= String(process)
+    Input.style.display = "none"
+    Submit.style.display = "none"
+    oldtimer = setTimeout(resetNumber, 5000)
+}
+produce()
+function GameResult() {
+    if (String(process)===Input.value) {
+        clearTimeout(oldtimer)
+        loss.textContent=""
+        currentLevel=currentLevel+1
+        level.textContent= "Level: "+currentLevel
+        produce()
+    }
+    else {
+        clearTimeout(oldtimer)
+        loss.textContent="You Lose!"
+    }
+}
+Submit.addEventListener("click", GameResult)
